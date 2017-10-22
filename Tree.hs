b3079dbcb82075929436cee5fc33e4aa582ffee3
Andronik Ordian <write@reusable.software>
2017-10-22 15:45:08 +0300

    add hw8 template

---
+++ Tree.hs
@@ -0,0 +1,20 @@
+import Prelude hiding (lookup)
+
+-- Implement binary search tree (4 points)
+-- 2 extra points for a balanced tree
+data BinaryTree k v = undefined
+
+-- “Ord k =>” requires, that elements of type k are comparable
+-- Takes a key and a tree and returns Just value if the given key is present,
+-- otherwise returns Nothing
+lookup :: Ord k => k -> BinaryTree k v -> Maybe v
+lookup = undefined
+
+-- Takes a key, value and tree and returns a new tree with key/value pair inserted.
+-- If the given key was already present, the value is updated in the new tree.
+insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
+insert = undefined
+
+-- Returns a new tree without the given key
+delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
+delete = undefined
